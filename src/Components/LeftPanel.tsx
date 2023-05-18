b0c1a6ab9d6ac86f1bb8226d63651bb0f04c0c16
borimino <markusandreassen1994@gmail.com>
2023-05-18 21:45:45 +0200

    Added save and load buttons. Don't do anything yet

--- src/Components/LeftPanel.tsx
+++ src/Components/LeftPanel.tsx
@@ -4,21 +4,28 @@ import { Canvas, useFrame, ThreeElements } from '@react-three/fiber'
 import { OrthographicCamera } from '@react-three/drei'
 import OrientationCube from './OrientationCube'
 import CameraHandler from './CameraHandler'
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 
 function LeftPanel(props: {cameraPosition: THREE.Vector3, onClick: (position: THREE.Vector3) => void}) {
   return (
     <div className="LeftPanel">
-        <div className="OrientationCube">
-            <Canvas orthographic camera={{ zoom: 50, position: props.cameraPosition }} onContextMenu={(e) => e.preventDefault()}>
-                <CameraHandler cameraPosition={props.cameraPosition} />
-                <ambientLight />
-                <directionalLight position={[1, 2, 0]} />
-                <OrientationCube onClick={props.onClick} />
-            </Canvas>
-        </div>
-        <p>
-            Left panel
-        </p>
+      <div className="OrientationCube">
+        <Canvas orthographic camera={{ zoom: 50, position: props.cameraPosition }} onContextMenu={(e) => e.preventDefault()}>
+          <CameraHandler cameraPosition={props.cameraPosition} />
+          <ambientLight />
+          <directionalLight position={[1, 2, 0]} />
+          <OrientationCube onClick={props.onClick} />
+        </Canvas>
+      </div>
+      <p className="LeftPannelMid">
+        Left panel
+      </p>
+      <div className="SaveLoadMenu">
+        <TextField id="fileName" label="Filename.zip"/>
+        <Button variant="contained">Load</Button>
+        <Button variant="contained">Save</Button>
+      </div>
     </div>
   );
 }
